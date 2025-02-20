0238f7d953155d224a7e113fdcc1584a04e875e9
임승현 <96limshyun@gmail.com>
2025-02-20 17:33:03 +0900

    feat: dashBoard CRUD 기능 구현

--- app/dashboard/edit/[id]/page.tsx
+++ app/dashboard/edit/[id]/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import EditDashboard from "@/app/ui/dashboard/Edit";
 import { use } from "react";
 import { Params } from "@/app/_types";
+import { useSearchParams } from "next/navigation";
 
 export default function EditPage({ params }: { params: Promise<Params> }) {
     const unwrappedParams = use(params);
     const id = unwrappedParams.id;
 
-    return <EditDashboard id={id} />;
+    const searchParams = useSearchParams();
+    const name = searchParams.get("name") || "";
+    const position = Number(searchParams.get("position")) || 0;
+
+    return (
+        <EditDashboard id={id} initialName={name} initialPosition={position} />
+    );
 }
--- app/ui/dashboard/MenuButton.tsx
+++ app/ui/dashboard/MenuButton.tsx
@@ -2,15 +2,20 @@ import { useState, useRef } from "react";
 import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
 import useOnClickOutside from "@/app/_hooks/useOnClickOutside";
+import { deleteDashboard } from "@/app/lib/dashBoardAction";
 interface MenuButtonProps {
     id: string;
+    name: string;
+    position: number;
 }
 
-export default function MenuButton({ id }: MenuButtonProps) {
+export default function MenuButton({ id, name, position }: MenuButtonProps) {
     const [isOpen, setOpen] = useState(false);
     const menuRef = useRef<HTMLDivElement>(null);
     useOnClickOutside(menuRef, () => setOpen(false));
-    
+
+    const deleteDashboardWithId = deleteDashboard.bind(null, id);
+
     return (
         <div ref={menuRef} className="relative">
             <button onClick={() => setOpen(!isOpen)}>
@@ -18,26 +23,21 @@ export default function MenuButton({ id }: MenuButtonProps) {
             </button>
 
             {isOpen && (
-                <div
-                    className="absolute top-full mt-2 right-0 w-20 bg-defaultCard shadow-lg border rounded-md z-50 flex flex-col text-sm text-center font-bold"
-                >
+                <div className="absolute top-full mt-2 right-0 w-20 bg-defaultCard shadow-lg border rounded-md z-50 flex flex-col text-sm text-center font-bold">
                     <Link
-                        href={`/dashboard/edit/${id}`}
+                        href={`/dashboard/edit/${id}?name=${name}&position=${position}`}
                         className="p-2 hover:bg-gray-500"
                         onClick={() => setOpen(false)}
                         scroll={false}
                     >
                         Edit
                     </Link>
-                    <button
+                    <form
+                        action={deleteDashboardWithId}
                         className="p-2 hover:bg-gray-500 text-red-500"
-                        onClick={() => {
-                            console.log("삭제 기능 추가 예정");
-                            setOpen(false);
-                        }}
                     >
-                        Delete
-                    </button>
+                        <button>Delete</button>
+                    </form>
                 </div>
             )}
         </div>
