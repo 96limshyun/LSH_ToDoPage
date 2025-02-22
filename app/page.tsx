dc68b365a78a2085c2c7178b031ac03a6d7c9781
임승현 <96limshyun@gmail.com>
2025-02-22 17:04:42 +0900

    refactor: fetchDashboards 로직 상단으로 변경 및 메인 title page로 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import DashboardsWrapper from "./ui/dashboard/DashboardWrap";
+import fetchDashboards from "./lib/data";
+import DashboardList from "./ui/dashboard/DashboardList";
 
-export default function Home() {
+export default async function Page() {
+    const dashboards = await fetchDashboards();
+
     return (
-        <main className="w-full h-full p-3">
-            <DashboardsWrapper />
-        </main>
+        <>
+            <div className="w-full">
+                <h1 className="font-bold text-2xl">LSH TO DO PAGE</h1>
+            </div>
+            <main className="w-full h-full p-3">
+                <DashboardList dashboards={dashboards} />
+            </main>
+        </>
     );
 }
