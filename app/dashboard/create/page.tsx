152b0179b58a1e1b3d000fae1fa7aa1b284b86ba
임승현 <96limshyun@gmail.com>
2025-02-22 17:17:18 +0900

    refactor: dashBoard에서 Card, Form 으로 분리된 컴포넌트 적용

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,5 +1,22 @@
-import CreateDashboard from "@/app/ui/dashboard/Create";
+import Card from "@/app/ui/Card";
+import { FORM_CARD_TITLES, INPUT_NAME, OK_BUTTON_TEXT } from "@/app/_constants";
+import useFormAction from "@/app/_hooks/useFormAction";
+import { createDashboard } from "@/app/lib/dashBoardAction";
+import Form from "@/app/ui/Form";
 
-export default function CreatePage() {
-    return <CreateDashboard />;
+export default function Page() {
+    const { state, formAction } = useFormAction((formData) =>
+        createDashboard(formData)
+    );
+    return (
+        <Card title={FORM_CARD_TITLES.CREATE_DASHBOARD}>
+            <Form
+                state={state}
+                formAction={formAction}
+                inputName={INPUT_NAME.DASHBOARD}
+                initialValue=""
+                okButtonText={OK_BUTTON_TEXT.CREATE}
+            />
+        </Card>
+    );
 }
