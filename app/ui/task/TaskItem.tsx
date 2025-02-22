244843ea06727f0e969a4798913f9c3ed80b024c
임승현 <96limshyun@gmail.com>
2025-02-22 17:19:14 +0900

    refactor: dashBoard와 task 컴포넌트 관심사 분리

--- app/ui/task/TaskItem.tsx
+++ app/ui/task/TaskItem.tsx
@@ -1,5 +1,6 @@
 import { Task } from "@/app/_types/dashboardType";
-import MenuButton from "./MenuButton";
+import { deleteTask } from "@/app/lib/taskAction";
+import Menu from "../Menu";
 
 interface TaskItemProps {
     task: Task;
@@ -7,10 +8,12 @@ interface TaskItemProps {
 
 export default function TaskItem({ task }: TaskItemProps) {
     const { id, content, position } = task;
+    const deleteTaskWithId = deleteTask.bind(null, id);
+    
     return (
         <div className="p-4 bg-defaultCard rounded border-[0.5px] flex justify-between">
             <div>{content}</div>
-            <MenuButton id={id} content={content} position={position} />
+            <Menu path={`/task/edit/${id}?content=${content}&position=${position}`} deleteAction={deleteTaskWithId} />
         </div>
     );
 }
---
+++ app/ui/task/TaskList.tsx
@@ -0,0 +1,41 @@
+import { Task } from "@/app/_types/dashboardType";
+import { Draggable, Droppable } from "@hello-pangea/dnd";
+import TaskItem from "./TaskItem";
+
+interface TaskListProps {
+    dashBoardId: string;
+    tasks: Task[]
+}
+
+export default function TaskList({ dashBoardId, tasks}: TaskListProps) {
+    return (
+        <Droppable droppableId={dashBoardId} type="TASK">
+            {(provided) => (
+                <div
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className="flex flex-col gap-2 min-h-[50px]"
+                >
+                    {tasks.map((task, index) => (
+                        <Draggable
+                            key={task.id}
+                            draggableId={task.id}
+                            index={index}
+                        >
+                            {(provided) => (
+                                <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                >
+                                    <TaskItem task={task} />
+                                </div>
+                            )}
+                        </Draggable>
+                    ))}
+                    {provided.placeholder}
+                </div>
+            )}
+        </Droppable>
+    );
+}
